b3da97a3dd1c55d3275ebef7a04fe97ca7d89b90
Francisco Mestizo <A01731549@itesm.mx>
2023-05-07 15:21:36 -0600

    Creacion del archivo para la funcion de practica

---
+++ Ensena-App/practiceLogic.swift
@@ -0,0 +1,23 @@
+//
+//  practiceLogic.swift
+//  Ensena-App
+//
+//  Created by Alumno on 03/05/23.
+//
+
+import Foundation
+
+func getPractice(course:String) -> [String] {
+    var words: [Palabra]
+    words = cargaPalabras(course: course)
+    
+    //var questionList = []
+
+    for word in words{
+        //word.
+        word
+    }
+                
+    
+    return []
+}
